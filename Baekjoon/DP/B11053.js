0cd0bb26cd3dc0b86c2fc2571cccec3733a82d6b
jylee <wjddp3@naver.com>
2021-08-14 00:04:22 +0900

    [백준] #11053 가장 긴 증가하는 부분 수열

---
+++ Baekjoon/DP/B11053.js
@@ -0,0 +1,33 @@
+/** [Baekjoon] #11053 - 가장 긴 증가하는 부분 수열
+ * Created by jylee on 2021-08-08
+ */
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const N = Number(input[0]);
+  const nums = input[1].split(" ").map(Number);
+  solution(N, nums);
+});
+
+function solution(N, nums) {
+  //dp[i]는 i인덱스까지 증가하는 수열의 개수를 의미한다.
+  let dp = Array(N).fill(1);
+
+  for (let i = 1; i < N; i++) {
+    //시작 인덱스부터 현재 인덱스까지 검사한다.
+    //이전 값이 현재 값보다 작으면, 현재 dp 값보다 이전 dp 누적값에 +1 한게 더 큰지 확인한다.
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  console.log(Math.max(...dp));
+}
